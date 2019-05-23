1a5d290cc8a9ef63100ef0b044da706223b32c74
Pierce <spammermuch@gmail.com>
2019-05-22 20:35:45 -0400

    remove event.js

--- components/events.js
+++
@@ -1,7 +0,0 @@
-const event = new (require('events')).EventEmitter();
-event.on('newListener', event => {
-    if(event === 'start') {
-        process.emitWarning('The \'start\' event has been removed. Use \'data\' instead.', 'DeprecationWarning');
-    }
-});
-module.exports = event;
