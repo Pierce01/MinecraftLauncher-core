6ca014dbba7b68b5c0645bc7e462c985c60611e0
HerozDotExe <herozdotexe@gmail.com>
2023-06-17 19:23:19 +0200

    Fix no artifact property when downloading a library

--- components/handler.js
+++ components/handler.js
@@ -509,13 +509,26 @@ class Handler {
         jarPath = path.join(directory, `${lib[0].replace(/\./g, '/')}/${lib[1]}/${lib[2]}`)
       }
 
-      if (!fs.existsSync(path.join(jarPath, name)) || !this.checkSum(library.downloads.artifact.sha1, path.join(jarPath, name))) {
+      // Fix no artifact property when downloading a library
+      if (!fs.existsSync(path.join(jarPath, name))) {
         // Simple lib support, forgot which addon needed this but here you go, Mr special.
         if (library.url) {
           const url = `${library.url}${lib[0].replace(/\./g, '/')}/${lib[1]}/${lib[2]}/${name}`
           await this.downloadAsync(url, jarPath, name, true, eventName)
         } else if (library.downloads && library.downloads.artifact) {
           await this.downloadAsync(library.downloads.artifact.url, jarPath, name, true, eventName)
+        }
+      }
+
+      if(library.downloads){
+        if(!this.checkSum(library.downloads.artifact.sha1, path.join(jarPath, name))){
+          // Simple lib support, forgot which addon needed this but here you go, Mr special.
+          if (library.url) {
+            const url = `${library.url}${lib[0].replace(/\./g, '/')}/${lib[1]}/${lib[2]}/${name}`
+            await this.downloadAsync(url, jarPath, name, true, eventName)
+          } else if (library.downloads && library.downloads.artifact) {
+            await this.downloadAsync(library.downloads.artifact.url, jarPath, name, true, eventName)
+          }
         }
       }
 
