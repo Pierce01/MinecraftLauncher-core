7efae84110088970c3c32edad052e4092510a03b
PadowYT2 <me@padow.ru>
2024-02-27 22:19:00 +0300

    Testing conditions

---
+++ test.js
@@ -0,0 +1,18 @@
+const { Client, Authenticator } = require(".");
+const launcher = new Client();
+
+launcher.launch({
+  authorization: Authenticator.getAuth("username"),
+  root: "./minecraft",
+  version: {
+    number: "1.7.10",
+    type: "release",
+  },
+  memory: {
+    max: "4G",
+    min: "2G",
+  },
+});
+
+launcher.on("debug", (e) => console.log(e));
+launcher.on("data", (e) => console.log(e));
