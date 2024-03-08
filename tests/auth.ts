6dffaab65c07364121d809d2cc1399a609dd756f
PadowYT2 <me@padow.ru>
2024-03-08 15:41:29 +0300

    Whoops

--- tests/auth.ts
+++ tests/auth.ts
@@ -1,7 +1,7 @@
 import { offline } from '../src';
 
 describe('Testing the offline authentication', () => {
-    test('Using Pierce as THE username', () => {
+    test('Using Pierce as the username', () => {
         expect(offline('Pierce')).toStrictEqual({
             access_token: '882661dc-e54f-35ae-b9e7-6c691a8095cb',
             client_token: '882661dc-e54f-35ae-b9e7-6c691a8095cb',
