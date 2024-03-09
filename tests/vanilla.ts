7d1ba38ef8b461616464ef8fc014ccbe200920b3
PadowYT2 <me@padow.ru>
2024-03-09 19:46:34 +0300

    Legacy and modern versions testing

--- tests/vanilla.ts
+++ tests/vanilla.ts
@@ -3,9 +3,41 @@ import { existsSync } from 'node:fs';
 import { resolve } from 'node:path';
 import { Client, Config } from '../src';
 
-Config.setConfig('version', { number: '1.14.4', type: 'release' });
+describe('Minecraft Vanilla Legacy (1.8.9)', () => {
+    Config.setConfig('version', { number: '1.8.9', type: 'release' });
 
-describe('Minecraft Vanilla', () => {
+    test(
+        'Installation',
+        async () => {
+            await Client.install();
+            expect(existsSync(resolve('./minecraft/cache/json/1.8.9.json'))).toBe(true);
+            expect(existsSync(resolve('./minecraft/natives/1.8.9'))).toBe(true);
+            expect(existsSync(resolve('./minecraft/versions/1.8.9/1.8.9.jar'))).toBe(true);
+        },
+        3 * 60 * 1000,
+    );
+
+    test(
+        'Starting',
+        async () => {
+            const process = await Client.start();
+            expect(process && typeof process.kill === 'function').toBe(true);
+            await new Promise<void>((resolve) => {
+                setTimeout(() => {
+                    (process as ChildProcessWithoutNullStreams).kill();
+                    resolve();
+                }, 10 * 1000);
+            });
+            expect(existsSync(resolve('./minecraft/saves'))).toBe(true);
+            expect(existsSync(resolve('./minecraft/resourcepacks'))).toBe(true);
+        },
+        3 * 60 * 1000,
+    );
+});
+
+describe('Minecraft Vanilla Modern (1.14.4)', () => {
+    Config.setConfig('version', { number: '1.14.4', type: 'release' });
+
     test(
         'Installation',
         async () => {
