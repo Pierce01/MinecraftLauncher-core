300d5c2d66c7ed2a7891d6da49e4bc226bafdd36
PadowYT2 <me@padow.ru>
2024-03-12 15:18:33 +0300

    Remake the exporting of `offline`

--- src/authenticator.ts
+++ src/authenticator.ts
@@ -1,7 +1,7 @@
 import { v3 } from 'uuid';
 import { User } from './utils/types';
 
-export default (username: string): User => {
+export const offline = (username: string): User => {
     const uuid = v3(username, v3.DNS);
     return { access_token: uuid, client_token: uuid, uuid, name: username, user_properties: '{}' };
 };
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * as Client from './launcher';
-export { default as offline } from './authenticator';
+export { offline } from './authenticator';
 export * from './utils/config';
 export { onLog } from './utils/log';
