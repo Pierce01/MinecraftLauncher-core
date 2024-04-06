76d8370bc7469fd6d1b300e3d43ff232470e2d1d
PadowYT2 <me@padow.ru>
2024-04-06 22:46:42 +0300

    Use rimraf

--- clean.js
+++
@@ -1,5 +0,0 @@
-const { existsSync, rm } = require('node:fs');
-
-// A simple way to delete the `build` folder
-// instead of `rm -rf` which is limited per OS
-if (existsSync('build')) rm('build', { recursive: true, force: true }, () => void 0);
